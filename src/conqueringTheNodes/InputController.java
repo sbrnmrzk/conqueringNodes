b17143b00ee0bd451e31ee79c23de034140549e7
Sabrina Marzuki <sbrnmrzk@gmail.com>
2020-06-10 05:28:44 +0800

    Input-GUI: Initial commit

--- src/conqueringTheNodes/InputController.java
+++ src/conqueringTheNodes/InputController.java
@@ -10,8 +10,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -34,7 +37,23 @@ public class InputController implements Initializable {
         int n = Integer.parseInt(this.n.getText());
         int t = Integer.parseInt(this.t.getText());
         int m = Integer.parseInt(this.m.getText());
+        System.out.println("n: " + n +" t: " +t + " m: " +m);
         
+    Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    //50 hardcoded, should be 'n' input
+    Nodes nodes = new Nodes(n);
+    Points[] vertices = nodes.getPoints();
+    
+    //3 is t, 5 is m inputs hardcoded
+    Edges edges = new Edges(nodes, t, m);
+    int[][] connectors = edges.getEdges();
+
+    Graph<Points> graph = new UnweightedGraph<>(vertices, connectors);
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(new GraphView(graph), 1000, 1000); //should be 1000x1000 kan? (fariz)
+    stage.setTitle("ConqueringTheNodes"); // Set the stage title
+    stage.setScene(scene); // Place the scene in the stage
+    stage.show(); // Display the stage
     }
     
     @Override
