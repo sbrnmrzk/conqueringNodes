34ed56e9d25a31b32412ed9bc28ffb67e51ce0ef
Sabrina Marzuki <sbrnmrzk@gmail.com>
2020-06-11 01:31:13 +0800

    Added error handling

--- src/conqueringTheNodes/InputController.java
+++ src/conqueringTheNodes/InputController.java
@@ -12,8 +12,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -21,44 +21,56 @@ import javafx.stage.Stage;
  * @author Admin
  */
 public class InputController implements Initializable {
-    
+
     @FXML
     private TextField n;
-    
+
     @FXML
     private TextField t;
-    
+
     @FXML
     private TextField m;
-    
-    @FXML
-    private void changeScene(ActionEvent event) {
-        System.out.println("You clicked me!");
-        int n = Integer.parseInt(this.n.getText());
-        int t = Integer.parseInt(this.t.getText());
-        int m = Integer.parseInt(this.m.getText());
-        System.out.println("n: " + n +" t: " +t + " m: " +m);
-        
-    Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-    //50 hardcoded, should be 'n' input
-    Nodes nodes = new Nodes(n);
-    Points[] vertices = nodes.getPoints();
-    
-    //3 is t, 5 is m inputs hardcoded
-    Edges edges = new Edges(nodes, t, m);
-    int[][] connectors = edges.getEdges();
 
-    Graph<Points> graph = new UnweightedGraph<>(vertices, connectors);
-    // Create a scene and place it in the stage
-    Scene scene = new Scene(new GraphView(graph), 1000, 1000); //should be 1000x1000 kan? (fariz)
-    stage.setTitle("ConqueringTheNodes"); // Set the stage title
-    stage.setScene(scene); // Place the scene in the stage
-    stage.show(); // Display the stage
+    @FXML
+    private Text errorMessage;
+
+    int points, thread, time;
+
+    @FXML
+    private void buttonClick(ActionEvent event) {
+        try {
+            points = Integer.parseInt(this.n.getText());
+            thread = Integer.parseInt(this.t.getText());
+            time = Integer.parseInt(this.m.getText());
+            System.out.println("n: " + n + " t: " + t + " m: " + m);
+            this.changeScene(event);
+        } catch (Exception e) {
+            if (points < thread) {
+                errorMessage.setText("Number of points should be more than thread");
+            } else {
+                errorMessage.setText("Please enter valid input");
+            }
+        }
     }
-    
+
+    private void changeScene(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Nodes nodes = new Nodes(this.points);
+        Points[] vertices = nodes.getPoints();
+        Edges edges = new Edges(nodes, this.thread, this.time);
+        int[][] connectors = edges.getEdges();
+
+        Graph<Points> graph = new UnweightedGraph<>(vertices, connectors);
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(new GraphView(graph), 1000, 1000); //should be 1000x1000 kan? (fariz)
+        stage.setTitle("Graph of nodes"); // Set the stage title
+        stage.setScene(scene); // Place the scene in the stage
+        stage.show(); // Display the stage
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
